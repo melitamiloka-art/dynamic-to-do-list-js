ed21c581e45c50ca112c502d383abe74fe41899f
Melita <melitamiloka@gmail.com>
2025-11-26 17:01:43 +0100

    Add script.js

--- script.js
+++ script.js
@@ -2,10 +2,14 @@ document.addEventListener('DOMContentLoaded', () => {
 
     
     const addButton = document.getElementById('add-task-btn');
-    const taskInput = document.getElementById('task-input');
-    const taskList = document.getElementById('task-list');
 
     
+    const taskInput = document.getElementById('task-input');
+
+    
+    const taskList = document.getElementById('task-list');
+
+   
     function addTask() {
 
         
@@ -21,32 +25,35 @@ document.addEventListener('DOMContentLoaded', () => {
         const li = document.createElement('li');
         li.textContent = taskText;
 
-       
+        
         const removeBtn = document.createElement('button');
         removeBtn.textContent = "Remove";
-        removeBtn.className = 'remove-btn';
+        removeBtn.className = "remove-btn";
 
         
         removeBtn.onclick = () => {
             taskList.removeChild(li);
         };
 
+       
         li.appendChild(removeBtn);
 
-        
+       
         taskList.appendChild(li);
 
-       
+        
         taskInput.value = "";
     }
 
+    
     addButton.addEventListener('click', addTask);
 
+    
     taskInput.addEventListener('keypress', (event) => {
         if (event.key === 'Enter') {
             addTask();
         }
     });
 
-    addTask();
+    
 });
